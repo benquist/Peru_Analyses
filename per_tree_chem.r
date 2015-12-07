2c031551fc49828983a58d1dcc18d7c076c9ad84
ashenkin <al@shenkin.org>
2015-12-07 12:25:42 +0000

    add dbh to output dataframe

--- per_tree_chem.r
+++ per_tree_chem.r
@@ -6,4 +6,5 @@ photosyn = get_photosyn(con)
 library(plyr)
 
 per_tree_chem = ddply(photosyn, .(tree_id), summarize, mean_c_percent = mean(c_percent, na.rm = T),
-                                                       mean_n_percent = mean(n_percent, na.rm = T))
+                                                       mean_n_percent = mean(n_percent, na.rm = T),
+                                                       dbh = dbh[1])
