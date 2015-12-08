bffe5ab84a033255bf7cf58c501b1fad5d341c63
benquist <benquist@email.arizona.edu>
2015-12-08 14:21:13 -0700

    small changes to queries

--- per_tree_chem_2.R
+++ per_tree_chem_2.R
@@ -84,6 +84,13 @@ per_tree_chem = ddply(photosyn, .(tree_id), summarize,
                       mean_n_percent = mean(n_percent, na.rm = T),
                       dbh = dbh[1])
 
+# Chambasa sometimes sampled trees outside of the plot, or sampled small lianas.  
+# These "Individuals" are not sampled as part of the regular census, and hence do 
+# not show up in the #forestplots database, and should not be used when calculating stem
+# -weighted figures (though #their traits may be used to calculate means, etc).
+
+So, all_fp_trees just includes trees in the forestplots census. all_trees includes censused trees as well as those just sampled in chambasa.
+
 all_trees = get_trees(con)
 all_fp_trees = subset(all_trees, ! grepl("^I", all_trees$tree_code))
 all_trees_with_chem = join(all_fp_trees[,c("tree_id","plot_code","tree_code","fp_species_name","dbh")], per_tree_chem, by = "tree_id", type = "left")
